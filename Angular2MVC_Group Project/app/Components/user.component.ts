51a84904bc891100959307f9095bf1ee929904b3
Astha2746 <33336381+Astha2746@users.noreply.github.com>
2017-11-05 15:42:05 -0600

    Update user.component.ts

--- Angular2MVC_Group Project/app/Components/user.component.ts
+++ Angular2MVC_Group Project/app/Components/user.component.ts
@@ -1,144 +1,82 @@
-﻿import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { UserService } from '../Service/user.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
-import { IUser } from '../Model/user';
-import { DBOperation } from '../Shared/enum';
-import { Observable } from 'rxjs/Rx';
 import { Global } from '../Shared/global';
+import { IUser } from '../Model/user';
 
 @Component({
-    templateUrl: 'app/Components/user.component.html'
+    template: `<img src="../../images/users.png" style="text-align:center"/>
+<br/>
+<div name="userNameDiv">
+<label for="username">Username</label>
+<input #username type="text" name="username" required />
+</div>
+
+   <div name="passwordDiv">
+   <label for="password">Password</label>
+<input #password type="password"  name="password" required />
+</div>
+
+<div name="buttonDiv">
+<button (click)="login(username.value, password.value)"  class="btn btn-primary">Login</button>
+</div>
+<br/>
+    {{clickMessage }}`
 })
 
-export class UserComponent implements OnInit {
+export class HomeComponent implements OnInit {
 
-    @ViewChild('modal') modal: ModalComponent;
+    clickMessage = '';
+
     users: IUser[];
     user: IUser;
     msg: string;
     indLoading: boolean = false;
-    userFrm: FormGroup;
-    dbops: DBOperation;
-    modalTitle: string;
-    modalBtnTitle: string;
 
-    constructor(private fb: FormBuilder, private _userService: UserService) { }
+    constructor(private _userService: UserService) { }
 
     ngOnInit(): void {
-        this.userFrm = this.fb.group({
-            Id: [''],
-            FirstName: ['', Validators.required],
-            LastName: [''],
-            Gender: ['', Validators.required]
-        });
         this.LoadUsers();
     }
 
-    LoadUsers(): void {
-        this.indLoading = true;
-        this._userService.get(Global.BASE_USER_ENDPOINT)
-            .subscribe(users => { this.users = users; this.indLoading = false; },
-            error => this.msg = <any>error);
-    }
+    login(username: string, password: string) {
+        var userNameIsFound= false;
+        var passwordDoesNotMatch = false;
 
-    addUser() {
-        this.dbops = DBOperation.create;
-        this.SetControlsState(true);
-        this.modalTitle = "Add New User";
-        this.modalBtnTitle = "Add";
-        this.userFrm.reset();
-        this.modal.open();
-    }
-
-    editUser(id: number) {
-        this.dbops = DBOperation.update;
-        this.SetControlsState(true);
-        this.modalTitle = "Edit User";
-        this.modalBtnTitle = "Update";
-        this.user = this.users.filter(x => x.Id == id)[0];
-        this.userFrm.setValue(this.user);
-        this.modal.open();
-    }
-
-    deleteUser(id: number) {
-        this.dbops = DBOperation.delete;
-        this.SetControlsState(false);
-        this.modalTitle = "Confirm to Delete?";
-        this.modalBtnTitle = "Delete";
-        this.user = this.users.filter(x => x.Id == id)[0];
-        this.userFrm.setValue(this.user);
-        this.modal.open();
-    }
-
-    onSubmit(formData: any) {
-        this.msg = "";
-   
-        switch (this.dbops) {
-            case DBOperation.create:
-                this._userService.post(Global.BASE_USER_ENDPOINT, formData._value).subscribe(
-                    data => {
-                        if (data == 1) //Success
-                        {
-                            this.msg = "Data successfully added.";
-                            this.LoadUsers();
-                        }
-                        else
-                        {
-                            this.msg = "There is some issue in saving records, please contact to system administrator!"
-                        }
-                        
-                        this.modal.dismiss();
-                    },
-                    error => {
-                      this.msg = error;
-                    }
-                );
-                break;
-            case DBOperation.update:
-                this._userService.put(Global.BASE_USER_ENDPOINT, formData._value.Id, formData._value).subscribe(
-                    data => {
-                        if (data == 1) //Success
-                        {
-                            this.msg = "Data successfully updated.";
-                            this.LoadUsers();
-                        }
-                        else {
-                            this.msg = "There is some issue in saving records, please contact to system administrator!"
-                        }
-
-                        this.modal.dismiss();
-                    },
-                    error => {
-                        this.msg = error;
-                    }
-                );
-                break;
-            case DBOperation.delete:
-                this._userService.delete(Global.BASE_USER_ENDPOINT, formData._value.Id).subscribe(
-                    data => {
-                        if (data == 1) //Success
-                        {
-                            this.msg = "Data successfully deleted.";
-                            this.LoadUsers();
-                        }
-                        else {
-                            this.msg = "There is some issue in saving records, please contact to system administrator!"
-                        }
-
-                        this.modal.dismiss();
-                    },
-                    error => {
-                        this.msg = error;
-                    }
-                );
-                break;
+        for (var i = 0; i < this.users.length; i++)
+        {
+            if (this.users[i].LastName == username)
+            {
+                userNameIsFound = true;
+                //found user,match firstname with password
+                if (password == this.users[i].FirstName)
+                {
+                    
+                    this.clickMessage = 'Login successfully!';
+                    return;
+                }
+                else
+                {
+                    passwordDoesNotMatch = true;
+                }
+            }
 
         }
+
+        this.clickMessage = 'Login failed!';
+        if (userNameIsFound)
+            this.clickMessage = this.clickMessage + "Due to not matching password";
+        else
+            this.clickMessage = this.clickMessage + "User is not found";
+
     }
 
-    SetControlsState(isEnable: boolean)
+    LoadUsers(): void
     {
-        isEnable ? this.userFrm.enable() : this.userFrm.disable();
+        this.indLoading = true;
+        this._userService.get(Global.BASE_USER_ENDPOINT)
+            .subscribe(users => { this.users = users; this.indLoading = false; }
+            //, error => this.msg = <any>error
+            );
     }
+
 }
